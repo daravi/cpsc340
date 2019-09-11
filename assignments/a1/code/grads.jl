36f30ceb4403cd065f07b4dc2d07fee6836a9938
Pooya Daravi <daravi@alumni.ubc.ca>
2019-09-11 00:10:59 -0700

    Finish 3 and add Julia style

--- assignments/a1/code/grads.jl
+++ assignments/a1/code/grads.jl
@@ -3,10 +3,10 @@ using Test
 ### An example to get you started:
 
 # Function
-func0(x) = sum(x.^2)
+func0(x) = sum(x.^2);
 
 # Gradient
-grad0(x) = 2x
+grad0(x) = 2x;
 
 ### Function 1
 
@@ -15,16 +15,16 @@ function func1(x)
 	for x_i in x
 		f += x_i^3;
 	end
-	return f
+	return f;
 end
 
 function grad1(x)
 	n = length(x);
 	g = zeros(n);
 	for i in 1:n
-		g[i] = 3 * (x[i]^2)
+		g[i] = 3 * (x[i]^2);
 	end
-	return g
+	return g;
 end
 
 ### Function 2
@@ -33,20 +33,27 @@ func2(x) = prod(x)
 function grad2(x)
 	n = length(x);
 	g = zeros(n);
-	# Put gradient code here
-	return g
+	for i in 1:n
+		g[i] = prod(x) / x[i];
+	end
+	return g;
 end
 
 ### Function 3
-func3(x) = -sum(log.(1 .+ exp.(-x)))
+func3(x) = -sum(log.(1 .+ exp.(-x)));
 
 function grad3(x)
-	# Put gradient code here	
+	n = length(x);
+	g = zeros(n)
+	for i in 1:n
+		g[i] = exp(-x[i]) / (1 + exp(-x[i]));
+	end
+	return g;
 end
 
 ### A function to approximate the derivative numerically
 function numGrad(func,x)
-	n = length(x);
+	n = length(x);6
 	delta = 1e-6;
 	g = zeros(n);
 	fx = func(x);
@@ -58,7 +65,9 @@ function numGrad(func,x)
 	return g
 end
 
-@test grad0([10, 2]) ≈ numGrad(func0, [10 2]) atol=0.0001
-@test grad1([10, 2]) ≈ numGrad(func1, [10 2]) atol=0.0001
-# @test grad2([10, 2]) ≈ numGrad(func2, [10 2]) atol=0.0001
-# @test grad3([10, 2]) ≈ numGrad(func3, [10 2]) atol=0.0001
+@testset "Gradient calculations" begin
+	@test grad0([10, 2]) ≈ numGrad(func0, [10 2]) atol=0.0001
+	@test grad1([10, 2]) ≈ numGrad(func1, [10 2]) atol=0.0001
+	@test grad2([10, 2]) ≈ numGrad(func2, [10 2]) atol=0.0001
+	@test grad3([10, 2]) ≈ numGrad(func3, [10 2]) atol=0.0001
+end
